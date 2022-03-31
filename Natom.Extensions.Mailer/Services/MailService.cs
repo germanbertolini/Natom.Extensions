e39da78845ff4b3f68ccce0960e2d529d6a852ef
German Bertolini <german.bertolini@gmail.com>
2022-03-30 21:31:04 -0300

    Mailer: Se quita template Default

--- Natom.Extensions.Mailer/Services/MailService.cs
+++ Natom.Extensions.Mailer/Services/MailService.cs
@@ -25,32 +25,6 @@ namespace Natom.Extensions.Mailer.Services
             _configuration = (ConfigurationService)serviceProvider.GetService(typeof(ConfigurationService));
         }
 
-        public async Task EnviarEmailParaConfirmarRegistroAsync(Transaction transaction, string scope, Usuario usuario)
-        {
-            string subject = "Confirmar registración";
-            string appAddress = await _configuration.GetValueAsync($"{scope}.URL");
-            string productName = await _configuration.GetValueAsync("General.ProductName");
-            var dataBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new { s = usuario.SecretConfirmacion, e = usuario.Email }));
-            var data = Uri.EscapeDataString(Convert.ToBase64String(dataBytes));
-            string link = new Uri($"{appAddress}/users/confirm/{data}").AbsoluteUri;
-            string body = ResourceTemplates.Default;
-            body = body.Replace("$body$", String.Format("<h2>¡Bienvenido a " + productName + "!</h2><br/><p>Por favor, para <b>generar la clave de acceso al sistema</b> haga clic en el siguiente link: <a href='{0}'>{0}</a></p>", link));
-            await EnviarMailAsync(transaction, subject, body, usuario.Email, usuario.Nombre);
-        }
-
-        public async Task EnviarEmailParaRecuperarClaveAsync(Transaction transaction, string scope, Usuario usuario)
-        {
-            string subject = "Recuperar clave";
-            string appAddress = await _configuration.GetValueAsync($"{scope}.URL");
-            string productName = await _configuration.GetValueAsync("General.ProductName");
-            var dataBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new { s = usuario.SecretConfirmacion, e = usuario.Email }));
-            var data = Uri.EscapeDataString(Convert.ToBase64String(dataBytes));
-            string link = new Uri($"{appAddress}/users/confirm/{data}").AbsoluteUri;
-            string body = ResourceTemplates.Default;
-            body = body.Replace("$body$", String.Format("<h2>Recupero de clave " + productName + "</h2><br/><p>Por favor, para <b>recuperar la clave de acceso al sistema</b> haga clic en el siguiente link: <a href='{0}'>{0}</a></p>", link));
-            await EnviarMailAsync(transaction, subject, body, usuario.Email, usuario.Nombre);
-        }
-
         public async Task EnviarMailAsync(Transaction transaction, string asunto, string htmlBody, string emailDestinatario, string nombreDestinatario = null)
         {
             _logger.LogInfo(transaction?.TraceTransactionId, "Comienzo envío email", new { emailDestinatario, nombreDestinatario, asunto, htmlBody });
